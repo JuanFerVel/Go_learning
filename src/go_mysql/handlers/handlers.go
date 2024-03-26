3001d869690823a159c7d13f058616a570d162f0
Juan Fernando Velandia Galindo <juan.velandia@sistemasgyg.com.co>
2024-03-26 14:16:34 -0500

    Conexion con Mysql

--- src/go_mysql/handlers/handlers.go
+++ src/go_mysql/handlers/handlers.go
@@ -23,18 +23,89 @@ func ListContacts(db *sql.DB) {
 	fmt.Println("LISTA DE CONTACTOS")
 	fmt.Println("-------------------------------------------------")
 	for rows.Next() {
-		//Instancia del modelo Contyact
+		//Instancia del modelo Contact
 		contact := models.Contact{}
-		err := rows.Scan(&contact.Id, &contact.Name, &contact.Email, &contact.Phone)
+
+		var valueEmail sql.NullString
+
+		err := rows.Scan(&contact.Id, &contact.Name, &valueEmail, &contact.Phone)
 		if err != nil {
 			log.Fatal(err)
 		}
 
+		if valueEmail.Valid {
+			contact.Email = valueEmail.String
+		} else {
+			contact.Email = "No registra correo"
+		}
+
 		fmt.Printf("ID: %d, Nombre: %s, Email: %s, Phone: %s\n",
-			contact.Id,
-			contact.Name,
-			contact.Email,
-			contact.Phone)
+			contact.Id, contact.Name, contact.Email, contact.Phone)
+		fmt.Println("-------------------------------------------------")
 	}
 
+}
+
+//GetContactById traera un contacto  de la base de datos mediante un ID
+
+func GetContactById(db *sql.DB, contactId int) {
+	query := "SELECT * FROM contact WHERE id = ?"
+
+	row := db.QueryRow(query, contactId)
+
+	contact := models.Contact{}
+	var valueEmail sql.NullString
+
+	err := row.Scan(&contact.Id, &contact.Name, &valueEmail, &contact.Phone)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			log.Fatalf("Registro no encontrado con el id %d", contactId)
+
+		} else {
+			log.Fatal(err)
+		}
+	}
+
+	if valueEmail.Valid {
+		contact.Email = valueEmail.String
+	} else {
+		contact.Email = "No registra correo"
+	}
+	fmt.Println("LISTA DE UN CONTACTO")
+	fmt.Println("-------------------------------------------------")
+	fmt.Printf("ID: %d, Nombre: %s, Email: %s, Phone: %s\n",
+		contact.Id, contact.Name, contact.Email, contact.Phone)
+	fmt.Println("-------------------------------------------------")
+
+}
+
+func CreateContact(db *sql.DB, contact models.Contact) {
+	query := "INSERT INTO contact(name, email, phone) VALUES(?, ?, ?)"
+
+	_, err := db.Exec(query, contact.Name, contact.Email, contact.Phone)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Contacto Registrado con Exito")
+}
+
+func UpdateContact(db *sql.DB, contact models.Contact) {
+	query := "UPDATE contact SET name = ?, email= ?, phone = ? WHERE id = ?"
+
+	_, err := db.Exec(query, contact.Name, contact.Email, contact.Phone, contact.Id)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("El registro se ha actualizado correctamente")
+}
+
+func DeleteContact(db *sql.DB, contactID int) {
+	query := "DELETE FROM contact WHERE id = ?"
+	_, err := db.Exec(query, contactID)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Contacto Eliminado con exito")
 }
--- src/go_mysql/main.go
+++ src/go_mysql/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"go_mysql/database"
 	"go_mysql/handlers"
+	"go_mysql/models"
 	"log"
+	"os"
+	"strings"
 
 	_ "github.com/go-sql-driver/mysql" // la "_" es porque se esta usando la libreria de forma indirecta
 )
@@ -15,5 +20,86 @@ func main() {
 	}
 	defer db.Close()
 
-	handlers.ListContacts(db)
+	for {
+		var option int
+		fmt.Print("Menú: \n",
+			"1. Listar Contactos \n",
+			"2. Obtener un Contacto por id \n",
+			"3. Crear un nuevo contacto \n",
+			"4. Actualizar un contacto \n",
+			"5.	Eliminar un contacto \n",
+			"6. Salir \n",
+			"Seleccione una opción: ")
+		fmt.Scanln(&option)
+		switch option {
+		case 1:
+			handlers.ListContacts(db)
+
+		case 2:
+			var idSeacrh int
+			fmt.Print("\n Digite el ID del registro que desea Buscar: ")
+			fmt.Scanln(&idSeacrh)
+			handlers.GetContactById(db, idSeacrh)
+
+		case 3:
+			newContact := inputContactDetails(option)
+			handlers.CreateContact(db, newContact)
+			handlers.ListContacts(db)
+
+		case 4:
+			newContact := inputContactDetails(option)
+			handlers.UpdateContact(db, newContact)
+			handlers.ListContacts(db)
+
+		case 5:
+			var idSeacrh int
+			fmt.Print("\n Digite el ID del registro que desea Eliminar: ")
+			fmt.Scanln(&idSeacrh)
+			handlers.DeleteContact(db, idSeacrh)
+		case 6:
+			return
+		default:
+			fmt.Println("Opcion no valida, intente de nuevo")
+		}
+	}
+
+	// handlers.ListContacts(db)
+
+	// handlers.GetContactById(db, 4)
+
+	// newContact := models.Contact{
+	// 	Id:    4,
+	// 	Name:  "Juan Desalcachofa",
+	// 	Email: "juan@gmail.com",
+	// 	Phone: "1234567890",
+	// }
+	// handlers.UpdateContact(db, newContact)
+	// handlers.CreateContact(db, newContact)
+	// handlers.DeleteContact(db, 5)
+
+}
+
+func inputContactDetails(option int) models.Contact {
+	reader := bufio.NewReader(os.Stdin)
+
+	var contact models.Contact
+
+	if option == 4 {
+		fmt.Print("\n Digite el ID del contacto que desea actualizar: ")
+		fmt.Scanln(&contact.Id)
+	}
+
+	fmt.Print("\n Digite el Nombre del contacto: ")
+	name, _ := reader.ReadString('\n')
+	contact.Name = strings.TrimSpace(name)
+
+	fmt.Print("\n Digite el Email del contacto: ")
+	email, _ := reader.ReadString('\n')
+	contact.Email = strings.TrimSpace(email)
+
+	fmt.Print("\n Digite el Telefono del contacto: ")
+	phone, _ := reader.ReadString('\n')
+	contact.Phone = strings.TrimSpace(phone)
+
+	return contact
 }
