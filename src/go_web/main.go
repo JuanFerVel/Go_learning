97bef2dda982c4c7597cdf14aba7e1e67b7fe6e0
Juan Fernando Velandia Galindo <juan.velandia@sistemasgyg.com.co>
2024-04-05 09:02:51 -0500

    go_Mysql Conexion y manejo de datos localmente

--- src/go_web/main.go
+++ src/go_web/main.go
@@ -8,8 +8,26 @@ import (
 
 func main() {
 	db.Connect()
-	db.Ping()
-	fmt.Println(db.ExistTable("users"))
-	db.CreateTale(models.UserSchema, "users")
+	// db.Ping()
+	// fmt.Println(db.ExistTable("users"))
+	// db.CreateTale(models.UserSchema, "users")
+	db.TruncateTable("users")
+
+	user := models.CreateUser("Lucas Martinez", "Lumas123", "lumartines@gmail.com")
+	fmt.Println(user)
+
+	// users := models.ListUsers()
+	// fmt.Println(users)
+
+	// user := models.GetUser(2)
+	// user.UserName = "Juan"
+	// user.Password = "juan789"
+	// user.Email = "juan@gmail.com"
+	// user.Save()
+	// users := models.ListUsers()
+	// fmt.Println(users)
+	// user.Delete()
+	users := models.ListUsers()
+	fmt.Println(users)
 	db.Close()
 }
--- src/go_web/models/users.go
+++ src/go_web/models/users.go
@@ -1,11 +1,17 @@
 package models
 
+import (
+	"go_Web/db"
+)
+
 type User struct {
-	Id       int
+	Id       int64
 	UserName string
 	Password string
 	Email    string
 }
+
+type Users []User
 
 const UserSchema string = `CREATE TABLE users(
 	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
@@ -13,3 +19,69 @@ const UserSchema string = `CREATE TABLE users(
 	password VARCHAR(100) NOT NULL,
 	email VARCHAR(50),
 	create_data TIMESTAMP DEFAULT CURRENT_TIMESTAMP)`
+
+// Construir Usuario
+func NewUser(username, password, email string) *User {
+	user := &User{UserName: username, Password: password, Email: email}
+	return user
+}
+
+// Crear Usuario e inserta BD
+func CreateUser(username, password, email string) *User {
+	user := NewUser(username, password, email)
+	user.Save()
+	return user
+}
+
+// Insertar Registro
+func (user *User) insert() {
+	sql := "INSERT users SET username=?, password=?, email=?"
+	result, _ := db.Exec(sql, user.UserName, user.Password, user.Email)
+	user.Id, _ = result.LastInsertId()
+}
+
+// Listar Registros
+func ListUsers() Users {
+	sql := "SELECT id, username, password, email FROM users"
+	users := Users{}
+	rows, _ := db.Query(sql)
+
+	for rows.Next() {
+		user := User{}
+		rows.Scan(&user.Id, &user.UserName, &user.Password, &user.Email)
+		users = append(users, user)
+	}
+	return users
+}
+
+// Listar un unico registro
+func GetUser(id int) *User {
+	user := NewUser("", "", "")
+	sql := "SELECT id, username, password, email FROM users WHERE id=?"
+	rows, _ := db.Query(sql, id)
+	for rows.Next() {
+		rows.Scan(&user.Id, &user.UserName, &user.Password, &user.Email)
+	}
+	return user
+}
+
+// Editar Usuario
+func (user *User) update() {
+	sql := "UPDATE users SET username=?, password=?, email=? WHERE id=?"
+	db.Exec(sql, user.UserName, user.Password, user.Email, user.Id)
+}
+
+// Guardar o editar Registro
+func (user *User) Save() {
+	if user.Id == 0 {
+		user.insert()
+	} else {
+		user.update()
+	}
+}
+
+// Eliminar un registro
+func (user *User) Delete() {
+	sql := "DELETE FROM users WHERE id=?"
+	db.Exec(sql, user.Id)
+}
